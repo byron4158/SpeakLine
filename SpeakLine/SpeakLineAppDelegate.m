83bf586e3474ecf0ae7782209b058d8ee9a97c11
Byron Kwok <byron.kwok@icloud.com>
2013-10-31 03:01:33 -0700

    Initial SpeakLine commit (finished ch. 5)

--- SpeakLine/SpeakLineAppDelegate.m
+++ SpeakLine/SpeakLineAppDelegate.m
@@ -10,9 +10,49 @@
 
 @implementation SpeakLineAppDelegate
 
+@synthesize window = _window;
+@synthesize textField = _textField;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        //Logs can help the beginner understand
+        //is happening and hunt down bugs.
+        NSLog(@"init");
+        
+        //Create a new instance of NSSpeechSynthesizer
+        //with the default voice.
+        _speechSynth = [[NSSpeechSynthesizer alloc]
+                        initWithVoice:nil];
+    }
+    return self;
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
 }
+
+- (IBAction)stopIt:(id)sender
+{
+    NSLog(@"stopping");
+    [_speechSynth stopSpeaking];
+}
+
+- (IBAction)sayIt:(id)sender
+{
+    NSString * string = [_textField stringValue];
+    
+    //Is the string zero-length?
+    if ([string length] == 0) {
+        NSLog(@"string from %@ is of zero-length", _textField);
+        return;
+    }
+    [_speechSynth startSpeakingString:string];
+    NSLog(@"Have started to say: %@", string);
+}
+
+
 
 @end
